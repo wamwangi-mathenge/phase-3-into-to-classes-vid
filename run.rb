c37cb7541cb45c74fa643b07fc20ab325d15e45e
Brian <wamwangimathenge@gmail.com>
2023-02-08 15:30:43 +0300

    Initialization

--- run.rb
+++ run.rb
@@ -20,4 +20,40 @@ snoopy = Dog.new
 
 rex = Dog.new
 
+# binding.pry
+
+class Animal
+    attr_accessor :type
+
+    def initialize(type)
+        @type = type;
+    end
+end
+
+class Human
+    attr_accessor :race
+
+    def initialize(race)
+        @type = race;
+    end
+end
+
+class Student
+    attr_accessor :name
+
+    def initialize(name)
+        @type = name;
+    end
+end
+
+mammal = Animal.new("Mammal")
+
+african = Human.new("Negro")
+
+josiah = Student.new("Josiah Battyman")
+
+adhiambo = Student.new("Everline Adhiambo")
+
+adhiambo.name
+
 binding.pry
